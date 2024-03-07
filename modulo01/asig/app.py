c8b8032517a92c7ef24e1296d8242be220d77ef7
Andres M Tosado <atosado6608@arecibointer.edu>
2024-03-07 14:11:31 -0400

    Update my files

--- modulo01/asig/app.py
+++ modulo01/asig/app.py
@@ -1,6 +1,15 @@
-arreglo = [1, 2, 3, 4, 5]
+def Average(list):
+    return sum(lst) / len(lst)
 
-ultimo_elemento = arreglo.pop()  # Remueve el último elemento
-arreglo.insert(0, ultimo_elemento)  # Inserta el último elemento al principio del arreglo
+def promedio(lista_numeros):
+    suma = 0
+    ctr = 0
+    for valor in lista_numeros:
+        suma = suma + valor
+        ctr = ctr + 1
+    return(suma / len(lista_numeros))
+lst = [15, 9, 55, 41, 35, 20, 62, 49]
+average = Average(lst)
 
-print(f"Arreglo con los elementos rotados: {arreglo}")
+print(lst)
+print(f'El valor promedio de la lista es {promedio(lst)}')
